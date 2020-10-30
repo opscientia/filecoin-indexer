c10eb5ca8ebc31c04c7d56d43e244675b7bcade1
Bartosz Pieńkowski <pienkowb@gmail.com>
2020-10-30 02:53:54 +0100

    Use an error group instead of a wait group

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pressly/goose v2.6.0+incompatible
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 	gorm.io/driver/postgres v1.0.1
 	gorm.io/gorm v1.20.4
 )
--- indexing/fetcher_tasks.go
+++ indexing/fetcher_tasks.go
@@ -2,12 +2,12 @@ package indexing
 
 import (
 	"context"
-	"sync"
 
 	"github.com/figment-networks/indexing-engine/pipeline"
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/figment-networks/filecoin-indexer/client"
 )
@@ -41,39 +41,40 @@ func (t *MinerFetcherTask) Run(ctx context.Context, p pipeline.Payload) error {
 	payload.MinersPower = make([]*api.MinerPower, len(addresses))
 	payload.MinersFaults = make([]*bitfield.BitField, len(addresses))
 
-	wg := &sync.WaitGroup{}
+	eg, _ := errgroup.WithContext(ctx)
 
 	for i := range addresses {
-		wg.Add(1)
-		go fetchMiner(i, t.client.Miner, payload, wg)
+		func(index int) {
+			eg.Go(func() error {
+				return fetchMinerData(index, t.client.Miner, payload)
+			})
+		}(i)
 	}
 
-	wg.Wait()
-
-	return nil
+	return eg.Wait()
 }
 
-func fetchMiner(index int, mc client.MinerClient, p *payload, wg *sync.WaitGroup) {
-	defer wg.Done()
-
+func fetchMinerData(index int, mc client.MinerClient, p *payload) error {
 	address := p.MinersAddresses[index]
 
 	info, err := mc.GetInfo(address)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	power, err := mc.GetPower(address)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	faults, err := mc.GetFaults(address)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	p.MinersInfo[index] = info
 	p.MinersPower[index] = power
 	p.MinersFaults[index] = faults
+
+	return nil
 }
