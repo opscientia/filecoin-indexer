85dd2464232f4c730b0f193e2b6e59d480556442
Bartosz Pieńkowski <pienkowb@gmail.com>
2020-12-19 03:59:37 +0100

    Fetch transactions concurrently

--- indexing/transaction_fetcher_task.go
+++ indexing/transaction_fetcher_task.go
@@ -2,9 +2,12 @@ package indexing
 
 import (
 	"context"
+	"sync"
 
 	"github.com/figment-networks/indexing-engine/pipeline"
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/figment-networks/filecoin-indexer/client"
 )
@@ -27,25 +30,60 @@ func (t *TransactionFetcherTask) GetName() string {
 // Run performs the task
 func (t *TransactionFetcherTask) Run(ctx context.Context, p pipeline.Payload) error {
 	payload := p.(*payload)
-	tsk := payload.EpochTipset.Key()
 
-	for _, addr := range payload.MinersAddresses {
-		cids, err := t.client.Transaction.GetCIDsByAddress(addr, tsk, payload.currentHeight)
-		if err != nil {
-			return err
-		}
-		payload.TransactionsCIDs = append(payload.TransactionsCIDs, cids...)
+	eg, _ := errgroup.WithContext(ctx)
+	mutex := sync.Mutex{}
+
+	for _, minerAddress := range payload.MinersAddresses {
+		func(addr address.Address) {
+			eg.Go(func() error {
+				return fetchTransactionCIDs(addr, t.client, payload, &mutex)
+			})
+		}(minerAddress)
+	}
+
+	err := eg.Wait()
+	if err != nil {
+		return err
 	}
 
 	payload.TransactionsMessages = make([]*types.Message, len(payload.TransactionsCIDs))
 
-	for i, cid := range payload.TransactionsCIDs {
-		msg, err := t.client.Transaction.GetMessage(cid)
-		if err != nil {
-			return err
-		}
-		payload.TransactionsMessages[i] = msg
+	for i := range payload.TransactionsCIDs {
+		func(index int) {
+			eg.Go(func() error {
+				return fetchTransactionMessage(index, t.client, payload)
+			})
+		}(i)
 	}
+
+	return eg.Wait()
+}
+
+func fetchTransactionCIDs(addr address.Address, c *client.Client, p *payload, m *sync.Mutex) error {
+	tsk := p.EpochTipset.Key()
+
+	cids, err := c.Transaction.GetCIDsByAddress(addr, tsk, p.currentHeight)
+	if err != nil {
+		return err
+	}
+
+	m.Lock()
+	p.TransactionsCIDs = append(p.TransactionsCIDs, cids...)
+	m.Unlock()
+
+	return nil
+}
+
+func fetchTransactionMessage(index int, c *client.Client, p *payload) error {
+	cid := p.TransactionsCIDs[index]
+
+	msg, err := c.Transaction.GetMessage(cid)
+	if err != nil {
+		return err
+	}
+
+	p.TransactionsMessages[index] = msg
 
 	return nil
 }
